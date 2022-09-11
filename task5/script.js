54db1407d546560bd43ba3f6d0023d721cfe5c39
Halyna <vechirniahalyna@ukr.net>
2022-09-11 22:56:33 +0300

    task5 update

--- task5/script.js
+++ task5/script.js
@@ -2,13 +2,13 @@ let input = document.querySelector('#input');
 let text = document.querySelector('#duplicateField');
 let button = document.querySelector('#button');
 
-input.oninput = function() {
+input.addEventListener('input', function() {
     text.textContent = input.value;
-} 
+} ) 
 
-button.onclick = function(event) {
+button.addEventListener('click', function(event) {
     console.log(input.value);
     input.value = '';
     text.textContent = '';
     event.preventDefault()
-}
+})
