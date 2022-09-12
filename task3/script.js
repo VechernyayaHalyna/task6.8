34a71186870324366e928bd950190f17fda58698
Halyna <vechirniahalyna@ukr.net>
2022-09-12 20:52:31 +0300

    update new

--- task3/script.js
+++ task3/script.js
@@ -1,22 +1,41 @@
+// Введение переменных
+
 let trafficLightEl = document.querySelector('#trafficLight');
+let trafficLightEl1 = document.querySelector('#trafficLight1');
+let trafficLightEl2 = document.querySelector('#trafficLight2');
+let block = document.querySelector('.block');
+
+// Функция "зеленый свет"
 
 function makeGreen() {
     trafficLightEl.style.background = ('green');
-    trafficLightEl.removeEventListener('click', makeGreen);
-    trafficLightEl.addEventListener('click', makeYellow);
+    trafficLightEl1.style.background = ('black');
+    trafficLightEl2.style.background = ('black'); 
+    block.removeEventListener('click', makeGreen);
+    block.addEventListener('click', makeYellow);
 }
+
+// Функция "желтый свет"
 
 function makeYellow() {
-    trafficLightEl.style.background = ('yellow');   
-    trafficLightEl.removeEventListener('click', makeYellow);
-    trafficLightEl.addEventListener('click', makeRed);
+    trafficLightEl1.style.background = ('yellow');
+    trafficLightEl.style.background = ('black'); 
+    trafficLightEl2.style.background = ('black');  
+    block.removeEventListener('click', makeYellow);
+    block.addEventListener('click', makeRed);
 }
+
+// Функция "красный свет"
 
 function makeRed() {
-    trafficLightEl.style.background = ('red');   
-    trafficLightEl.removeEventListener('click', makeRed);
-    trafficLightEl.addEventListener('click', makeGreen);
+    trafficLightEl2.style.background = ('red');
+    trafficLightEl1.style.background = ('black');
+    trafficLightEl.style.background = ('black');   
+    block.removeEventListener('click', makeRed);
+    block.addEventListener('click', makeGreen);
 }
 
-trafficLightEl.addEventListener('click', makeGreen);
+// Первый клик
+
+block.addEventListener('click', makeGreen);
 
